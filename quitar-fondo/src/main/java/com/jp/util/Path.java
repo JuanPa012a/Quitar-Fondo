e2c482b1153b417bb608cbac49e3d5c1a2bd1520
JuanPa012a <1034776660juan@gmail.com>
2025-08-29 20:19:53 -0500

    importante!!1

--- quitar-fondo/src/main/java/com/jp/util/Path.java
+++ quitar-fondo/src/main/java/com/jp/util/Path.java
@@ -2,7 +2,7 @@ package com.jp.util;
 
 public class Path {
     
-    public static String MAIN = "/view/main.fxml";
+    public static String MAIN = "/view/Main.fxml";
     public static String STYLE = "/css/style.css";
     public static String ICON = "/icon/1144760.png";
 }
